517bb949454bf19fa37bfb5b598db2d9d8ce6f05
Kwae Htoo <kwaekh@gmail.com>
2023-10-23 10:54:47 -0700

    fixes to arrayExamples methods
    fixed reverseInPlace & reversed methods

--- ArrayTests.java
+++ ArrayTests.java
@@ -15,4 +15,24 @@ public class ArrayTests {
     int[] input1 = { };
     assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
   }
+
+  @Test
+  public void testReverseInPlaceTwo() {
+    int[] input1 = {1, 2, 3, 4, 5};
+    int[] output1 = {5, 4, 3, 2, 1};
+    ArrayExamples.reverseInPlace(input1);
+    assertArrayEquals(output1, input1);
+
+    int[] input2 = {1,2};
+    int[] output2 = {2,1};
+    ArrayExamples.reverseInPlace(input2);
+    assertArrayEquals(output2, input2);
+  }
+
+  @Test
+  public void testReversedTwo() {
+    int[] input1 = {1,2,3,4,5};
+    int[] output1 = {5,4,3,2,1};
+    assertArrayEquals(output1, ArrayExamples.reversed(input1));
+  }
 }
